4f8bf354fa7ded35703fc39e26751abffacbc666
ZUBENATHI-SAMKILE <samkilezubenathi@gmail.com>
2026-03-26 12:14:23 +0200

    Setup Supabase DB

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 import os
 import dj_database_url
 
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,17 +75,6 @@ if DATABASE_URL:
             conn_max_age=600,
             ssl_require=True,
         )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME", "library_db"),
-            "USER": os.getenv("DB_USER", "postgres"),
-            "PASSWORD": os.getenv("DB_PASSWORD", ""),
-            "HOST": os.getenv("DB_HOST", "localhost"),
-            "PORT": os.getenv("DB_PORT", "5432"),
-        }
     }
 
 AUTH_USER_MODEL = "users.User"
